6c67fdb1059b997ecc0b49ff333298551ee852e7
marco raschillà <marcoraschilla92@gmail.com>
2019-10-23 16:00:56 +0200

    marco

--- fed3-javascript/js/basic.js
+++ fed3-javascript/js/basic.js
@@ -1,7 +1,40 @@
-let target = document.getElementById('target');
+/*let target = document.getElementById('target');
 target.textContent = 'Hello!';
-console.log('hello!');
+console.log('hello!');  */
 
+
+function Person(first, last) {
+    this.first = first;
+    this.last = last;
+}
+let p = new Person('Tom', 'Jones');
+
+console.log(p);
+
+
+
+/* scrivere una funziona che ritorni true se il valore precedente è pari (isEven(n)), false se dispari. */
+
+
+
+/*
+console.log(isEven(7));          //false
+console.log(isEvan(12));         //true
+*/
+
+
+
+/* function isEvan()
+*/
+
+/*
+function isEven(n) {
+if
+
+
+}
+
+*/
 
 
 
