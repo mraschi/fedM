259130a04fef168df14771bdc64bd2372a2087c5
marco raschillà <marcoraschilla92@gmail.com>
2019-10-24 12:36:05 +0200

    mm

--- fed3-javascript/23-10.js
+++ fed3-javascript/23-10.js
@@ -25,17 +25,18 @@ console.log(isEven(2.5) === undefined)
 console.log(isEven("2") === undefined)
 
 
-console.log("-------------------------");
+console.log("---------------esercizio----------");
 
 function Person(first, last) {
     this.first = first;
     this.last = last;
 }
+
 let p = new Person('marco', 'raschillà');
 
 console.log(p);
 
-console.log("----------------------------")
+console.log("---------------esercizio-------------")
 
 
 
@@ -59,12 +60,50 @@ console.log(speed(-5, 50) === undefined);
 console.log(speed('ciao', 50) === undefined);
 
 
-console.log("----------------------------")
+console.log("--------------------esercizio--------")
 
 
 
 
 /* digitSum(number) ritorna la somma delle cifre (127 =>  10;    111 =>3;     -51 =>6);  number deve essere intero */
+
+
+function digitSum(number) {
+    let sum = 0;
+    while (number > 0) {
+        resto = number % 10;
+        sum = sum + resto;
+        number = (number - resto) / 10;
+    }
+    return sum;
+}
+
+
+console.log(digitSum(127) == 10);
+
+
+console.log("---------------------------esercizio------------")
+
+function digitSumNew(number) {
+    let sum = 0;
+    if ((typeof (number) != "number")) {
+        return undefined;
+    } else if (number <= 0) {
+        number = number * (-1);
+    }
+    while (number > 0) {
+        let resto = number % 10;
+        sum = sum + resto;
+        number = (number - resto) / 10;
+    }
+    return sum;
+}
+
+console.log(digitSumNew(-51) == 6);
+
+
+
+
 
 
 
