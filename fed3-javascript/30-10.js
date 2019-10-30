eaec93f342d6b036e2981f7f1712f530c384c5d6
marco raschillà <marcoraschilla92@gmail.com>
2019-10-30 10:25:08 +0100

    mm

---
+++ fed3-javascript/30-10.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+    let prev = 1;
+    let cur = 1;
+    for (i = 1; i <= n; ++i) {
+        temp = prev + cur;
+        prev = cur;
+        cur = temp;
+    }
+return cur;
+}
+console.log(fibonacci(13) ===377;
+
+
