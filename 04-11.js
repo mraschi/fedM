daf3b6fe920be07752c67a0daa45bdc4629ce008
marco raschillà <marcoraschilla92@gmail.com>
2019-11-05 17:42:18 +0100

    mm

--- 04-11.js
+++ 04-11.js
@@ -11,19 +11,75 @@ function check() {
 
 function changeBgcolor() {
     let color = document.getElementById("color").value;
-        document.bgColor = color;
+    document.bgColor = color;
 }
 
 function changeBgcolorSelect() {
     let color = document.getElementById("selectColor").value;
-        document.bgColor = color;
+    document.bgColor = color;
+}
+
+function changeOnMouse() {
+    let target = document.getElementById ("titoloOnMouse")
+    target.textContent = "Ciao";
+}
+
+
+
+function getInfo() {
+    let request = new XMLHttpRequest();
+    request.onload = callback;
+    request.open("GET", "tom.json");
+    request.send();
+}
+
+
+function callback() {
+    let target = document.getElementById('target');
+    if (this.status != 200) {
+        target.value += "[" + this.status + "]\n";
+        return;
+    }
+    let json = JSON.parse(this.responseText);
+    target.value += json.name + '\n';
+    target.value += json.job.title + '\n';
+    target.value += json.job.languages + '\n';
 }
 
 
 
 window.onload = (ev) => {
     let target = document.getElementById('target');
-    target.textContent = 'Hello';
+    target.textContent = 'Hello' ;
     console.log(ev);
 }
+
+
+
+
+
+
+
+
+function getInfoTXT() {
+    let request = new XMLHttpRequest();
+    request.onload = callbackTXT;
+    request.open("GET", "tom.txt");
+    request.send();
+}
+
+function callbackTXT() {
+    let target = document.getElementById('target');
+    if (this.status != 200) {
+        target.value += "[" + this.status + "]\n";
+        return;
+    }
+    let json = JSON.parse(this.responseText);
+    target.value += json.name + '\n';
+    target.value += json.job.title + '\n';
+    target.value += json.job.languages + '\n';
+}
+
+
+
 
