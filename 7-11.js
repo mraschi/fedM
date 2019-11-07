c3df14f225ff98a570a67237261ba2acd2bf33f0
marco raschillà <marcoraschilla92@gmail.com>
2019-11-07 12:37:25 +0100

    mm

---
+++ 7-11.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    document.getElementById("span").textContent = $("div").length;
+    $('<div>AIUTOOOO</div>').css({color: 'red'}).appendTo('body');
+
+});
+
+
+
+
+
+
