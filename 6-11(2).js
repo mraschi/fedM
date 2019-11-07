b3be67b194505588242670ddee3cb78001c564c0
marco raschillà <marcoraschilla92@gmail.com>
2019-11-07 11:38:56 +0100

    nmm

--- 6-11(2).js
+++ 6-11(2).js
@@ -1,38 +1,33 @@
-
-
-
-
-
-
-
-
-
-
-
-
- function filler() {
-    let fill = new XMLHttpRequest();
-    fill.onload = callback;
-    fill.open("GET", "tom.json");
-    fill.send();
+function filler() {
+    let request = new XMLHttpRequest();
+    request.onload = callback;
+    request.open("GET", "tom.json");
+    request.send();
 }
+
+
+
+
 
 
 function callback() {
-    let target = document.getElementById('target');
-    if (this.status != 200) {
-        target.value += "[" + this.status + "]\n";
-        return;
-    }
     let json = JSON.parse(this.responseText);
-    target.value += json.name + '\n';
-    target.value += json.job.title + '\n';
-    target.value += json.job.languages + '\n'; 
+    document.getElementById('name').value = json.name;
+    document.getElementById('job').value = json.job.title;
+    let lang = json.job.languages;
+    for (j = 0; j < lang.length; j++) {
+        if (lang[j] == "JavaScript") {
+            document.getElementById("langJS").checked = true;
+        } else if (lang[j] == "HTML") {
+            document.getElementById("langHtml").checked = true;
+        } else if (lang[j] == "CSS") {
+            document.getElementById("langCss").checked = true;
+        } else if (lang[j] == "Python") {
+            document.getElementById("langPy").checked = true;
+        } else if (lang[j] == "Java") {
+            document.getElementById("langJ").checked = true;
+        } else if (lang[j] == "Cpp") {
+            document.getElementById("langCpp").checked = true;
+        }
+    }
 }
-
-
-
-
-
-
-
