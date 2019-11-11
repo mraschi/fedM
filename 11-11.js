f7373f32ad4e9edf3d818b933113d14be359398a
marco raschillà <marcoraschilla92@gmail.com>
2019-11-11 10:45:17 +0100

    m

--- 11-11.js
+++ 11-11.js
@@ -1,4 +1,4 @@
-function Armstrong(value) {
+ //function Armstrong(value) { 
 
 
 
@@ -10,18 +10,17 @@ function Armstrong(value) {
 
 
 
+    function ordina(arr) {
+        arr.sort((a, b) => {
+            if (a == b) {
+                return 0;
+            } else if (a < b) {
+                return -1;
+            } else {
+                return +1;
+            }
+        });
+        return arr;
+    }
 
-
-console.log("----------------------------------------") 
-
-
-
-function Array(value) {
-array.sort
-
-
-
-
-
-
-
+    console.log(ordina([3, 5, 1, 7, 12]));
