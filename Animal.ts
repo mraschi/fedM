2c1a93282952315bede30f7adbd3ec767789bb30
marco raschillà <marcoraschilla92@gmail.com>
2019-11-11 16:59:26 +0100

    m

---
+++ Animal.ts
@@ -0,0 +1,43 @@
+class Animal {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+    brush() {
+        return "I am brushing" + this.name;
+    }
+}
+
+class Dog extends Animal {
+    race: string;
+    constructor(name: string, race: string) {
+        super(name);
+        this.race = race;
+    }
+    brush() {
+        return super.brush() + "of race" + this.race;
+    }
+}
+class Cat extends Animal {
+    race: string;
+    constructor(name: string, race: string) {
+        super(name);
+        this.race = race;
+    }
+    brush() {
+        return super.brush() + "of race" + this.race;
+    }
+}
+
+let array: Animal[] = []
+array.forEach {
+    (x) => {
+        console.log(x.brush());
+    }
+}
+
+
+
+
+
