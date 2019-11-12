af4fb01a8af6255b02327549ed9d5e617c002504
marco raschillà <marcoraschilla92@gmail.com>
2019-11-12 10:03:15 +0100

    mm

---
+++ fed4-typescript/hamming.js
@@ -0,0 +1,14 @@
+function hammingDistance(a, b) {
+    if (a.length !== b.length) {
+        throw new Error('le stringhe devono avere la stessa lunghezza');
+    }
+    var distance = 0;
+    for (var i = 0; i < a.length; i += 1) {
+        if (a[i] !== b[i]) {
+            distance += 1; // incremento di uno 
+        }
+    }
+    return distance;
+}
+console.log(hammingDistance(("1011101"), ("1001001")) == 2);
+console.log(hammingDistance(("2143896"), ("2233796")) == 3);
