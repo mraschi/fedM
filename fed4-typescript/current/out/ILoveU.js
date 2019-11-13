aec30d450447b6e9b5a2a3e22a37b38a78fe8034
marco raschillà <marcoraschilla92@gmail.com>
2019-11-13 17:26:48 +0100

    mm

---
+++ fed4-typescript/current/out/ILoveU.js
@@ -0,0 +1,2 @@
+//ciao
+//# sourceMappingURL=ILoveU.js.map
---
+++ fed4-typescript/out/current/ILoveU.js
@@ -0,0 +1,22 @@
+function BMI(weight, height) {
+    let BMI = weight / (Math.pow(height, 2));
+    let message = "";
+    if (BMI <= 18.5) {
+        message = "Underweight";
+    }
+    else if (BMI > 18.5 && BMI <= 25) {
+        message = 'Normal';
+    }
+    else if (BMI > 25 && BMI <= 30) {
+        message = "Overweight";
+    }
+    else if (BMI > 30) {
+        message = "Obese";
+    }
+    else {
+        message = "undefined";
+    }
+    return message;
+}
+console.log(BMI(75, 1.90));
+//# sourceMappingURL=ILoveU.js.map
