ebecdbe7d212ef7dbb7fc074bbe1e3394625a7a0
marco raschillà <marcoraschilla92@gmail.com>
2019-11-18 15:23:27 +0100

    mm

--- fed5-angular/ng/my-app2/src/app/app.module.ts
+++ fed5-angular/ng/my-app2/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { UserComponent } from './user/user.component';
 import { UsersComponent } from './users/users.component';
+import { AddItemComponent } from './add-item/add-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
