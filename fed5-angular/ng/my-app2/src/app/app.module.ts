5e641538aee3ff11e7e381a5ca8050d47c33c695
marco raschillà <marcoraschilla92@gmail.com>
2019-11-18 13:58:58 +0100

    mm

---
+++ fed5-angular/ng/my-app2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    UserComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
