2c538541dd8617b224c23f55579cad4e79551f9d
marco raschillà <marcoraschilla92@gmail.com>
2019-11-18 16:20:38 +0100

    marco

---
+++ fed5-angular/ng/ng-oved/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+component:string[];
+
+  title = 'ng-oved';
+}
